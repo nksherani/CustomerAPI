10f1ad03721c3d1689295134e1a180eaf7dfe2c4
naveed <nksherani@outlook.com>
2019-04-10 00:39:00 +0500

    no message

---
+++ dbo.Table.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Customers]
+(
+	[CustomerId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [CustomerName] VARCHAR(MAX) NULL, 
+    [Address] VARCHAR(MAX) NULL, 
+    [Zip] VARCHAR(MAX) NULL, 
+    [City] VARCHAR(MAX) NULL, 
+    [Telephone] VARCHAR(MAX) NULL, 
+    [ContactFirst] VARCHAR(MAX) NULL, 
+    [ContactLast] VARCHAR(MAX) NULL, 
+    [CreatedDate] DATETIME NULL, 
+    [UpdatedDate] DATETIME NULL
+)
